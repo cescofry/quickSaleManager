8222083c3a134911e884259a59c8d1631977cac2
cescofry <cescofry@gmail.com>
2017-02-17 16:11:52 -0800

    Change service port to 8080

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -11,7 +11,7 @@ router.get("/") {
 }
 
 // Add an HTTP server and connect it to the router
-Kitura.addHTTPServer(onPort: 80, with: router)
+Kitura.addHTTPServer(onPort: 8080, with: router)
 
 // Start the Kitura runloop (this call never returns)
 Kitura.run()
