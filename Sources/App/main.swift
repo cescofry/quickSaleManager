71f82550936468c02ae23c380b7a4cfd94602a66
cescofry <cescofry@gmail.com>
2017-02-22 15:59:03 -0800

    Making port dynamic based on proc input

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,6 +2,20 @@ import Kitura
 import LoggerAPI
 import HeliumLogger
 
+
+
+extension CommandLine {
+    static var port: Int? {
+        guard self.arguments.count < 2,
+            let port = Int(self.arguments[1])
+            else {
+                return nil
+        }
+        
+        return port
+    }
+}
+
 Log.logger = HeliumLogger()
 
 
@@ -15,11 +29,18 @@ router.get("/") {
     next()
 }
 
+let port = CommandLine.port
+if port == nil {
+    Log.exit("Exiting ... No port specified")
+}
+
 // Add an HTTP server and connect it to the router
-let server = Kitura.addHTTPServer(onPort: 8080, with: router)
+let server = Kitura.addHTTPServer(onPort: port!, with: router)
 
 // Start the Kitura runloop (this call never returns)
 
 Kitura.run()
 
 Log.exit("Exiting ... (server state: \(server.state))")
+
+
